347b855460b59f39c6aafdebce1075d90e3d637c
stevennuo <3er.com.cn@gmail.com>
2014-03-23 00:48:51 +0800

    fix ant release error.

--- src/org/restlet/representation/ObjectRepresentation.java
+++ src/org/restlet/representation/ObjectRepresentation.java
@@ -33,8 +33,8 @@
 
 package org.restlet.representation;
 
-import java.beans.XMLDecoder;
-import java.beans.XMLEncoder;
+//import java.beans.XMLDecoder;
+//import java.beans.XMLEncoder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
